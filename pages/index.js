7b5af541d21e1e68a936c95687021a632c9dc041
Jhonathan Vitor <jhonathanvitor@gmail.com>
2022-08-31 17:29:26 -0400

    edit css global

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,22 @@
 import React from 'react'
 
-export const index = () => {
+const Home = () => {
   return (
-    <div>index</div>
+    <>
+      HeroBanner
+
+      <div>
+        <h2>Beset Selling Products</h2>
+        <p>Speakers of many variations</p>
+      </div>
+      <div>
+        {['Product 1', 'Product 2'].map(
+          (product) => product)}
+      </div>
+
+      Footer
+    </>
   )
 }
+
+export default Home;
