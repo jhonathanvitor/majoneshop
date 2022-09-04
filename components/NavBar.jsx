2b3bef27462d828feac427082390f2f8d1f4bd64
Jhonathan Vitor <jhonathanvitor@gmail.com>
2022-09-03 20:45:27 -0400

    product Slug

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShop } from 'react-icons/ai';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar-container'>
+      <p className="logo">
+        <Link href="/">Majones Shop</Link>
+      </p>
+
+      <button type='button' className='cart-icon' onClick="">
+        <AiOutlineShop />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
   )
 }
 
